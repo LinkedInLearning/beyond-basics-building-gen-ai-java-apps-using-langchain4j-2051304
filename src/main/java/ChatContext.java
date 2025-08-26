59ca63ff02a23ad6476d2ca4958f92681099bec1
Frank Greco <fgreco@javasig.com>
2025-08-25 22:25:09 -0400

    Lab 1 Demonstrate chatbot without context and with context

---
+++ src/main/java/ChatContext.java
@@ -0,0 +1,50 @@
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ChatContext {
+    public static void main(String[] args) {
+        Scanner userinput;
+        String cmdline;
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(100);
+        ChatMessage cm;
+
+        SystemMessage sysmsg = new SystemMessage("""
+                    You are a polite Java consultant with deep expertise in teaching AI and Machine Learning.
+                """);
+        chatMemory.add(sysmsg);
+
+        ChatModel cmodel = OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(OpenAiChatModelName.GPT_4_O)
+                .build();
+
+        while (true) {
+            System.out.print("prompt> ");
+
+            userinput = new Scanner(System.in);
+            cmdline = userinput.nextLine();
+
+            if (cmdline.isBlank())       // If nothing, do nothing
+                continue;
+
+            UserMessage usrmsg = UserMessage.from(cmdline);   // create the prompt
+            chatMemory.add(usrmsg);
+
+            var answer = cmodel.chat(chatMemory.messages());  // send the context as messages and save the response
+            var response = answer.aiMessage().text();
+
+            System.out.println(response);
+
+            chatMemory.add(UserMessage.from(response));     // Add the response from the assistant
+        }
+    }
+}
